1aad9e58b937425108b2352ec8eaaf1adae360d7
Skyllerz-IT <dadivarie@gmail.com>
2024-10-24 13:08:51 +0200

    pacchetto

--- SantiSbustati/PacketView.swift
+++ SantiSbustati/PacketView.swift
@@ -1,26 +1,33 @@
-//import SwiftUI
-//
-//struct LoopingAnimationView: View {
-//    @State private var isScaled = false  // State variable to trigger the animation
-//    @State private var isRotating = false
-//    var body: some View {
-//        VStack {
-//            Image(systemName: "star.fill")
-//                .resizable()
-//                .frame(width: 100, height: 100)
-//                .foregroundColor(.yellow)
-//                .scaleEffect(isScaled ? 2.0 : 1.75)  // Scale up and down
-//                .onAppear {
-//                    // Repeat animation forever with autoreverses to go back to original state
-//                    withAnimation(Animation.easeInOut(duration: 0.75).repeatForever(autoreverses: true)) {
-//                        isScaled.toggle()  // Toggle the state to start the animation
-//    
-//                    }
-//                }
-//        }
-//    }
-//}
-//
-//#Preview {
-//    LoopingAnimationView()
-//}
+import SwiftUI
+
+struct LoopingAnimationView: View {
+    @State private var isScaled = false  // State variable to trigger the animation
+
+    var body: some View {
+        VStack {
+            // Red rectangle (above the yellow one)
+            Rectangle()
+                .frame(width: 140, height: 30)
+                .foregroundColor(.red)
+                .scaleEffect(isScaled ? 2.0 : 1.5)  // Scale up and down
+                .onAppear {
+                    withAnimation(Animation.easeInOut(duration: 0.75).repeatForever(autoreverses: true)) {
+                        isScaled.toggle()  // Start animation
+                    }
+                }
+
+                .padding(.top, 20)
+
+            // Yellow rectangle (below the red one)
+            Rectangle()
+                .frame(width: 140, height: 200)
+                .foregroundColor(.yellow)
+                .scaleEffect(isScaled ? 2.0 : 1.3)  // Scale up and down
+        }
+        .padding()  // Add padding around the entire VStack
+    }
+}
+
+#Preview {
+    LoopingAnimationView()
+}
