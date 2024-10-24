64922f0558086cddd29f7c2cd86c7e769cc3daf0
Skyllerz-IT <dadivarie@gmail.com>
2024-10-24 10:21:59 +0200

    Initial Commit

---
+++ SantiSbustati/SantiSbustatiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SantiSbustatiApp.swift
+//  SantiSbustati
+//
+//  Created by Davide Picentino on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SantiSbustatiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SantiSbustatiTests/SantiSbustatiTests.swift
@@ -0,0 +1,17 @@
+//
+//  SantiSbustatiTests.swift
+//  SantiSbustatiTests
+//
+//  Created by Davide Picentino on 24/10/24.
+//
+
+import Testing
+@testable import SantiSbustati
+
+struct SantiSbustatiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
